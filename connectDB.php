faaa0aa476b9ca516051dbff2166e3b1f185f396
Steve Mafoa <steve_mafoa@icloud.com>
2025-04-15 19:07:59 +0200

    fix: Connexion  réussi à la bdd mis en commentaire pour plus de propreté

--- connectDB.php
+++ connectDB.php
@@ -8,7 +8,7 @@ try {
 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie ! <br>";
+    // echo "Connexion réussie ! <br>";
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
 
